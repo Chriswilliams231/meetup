1e03a3d8de4bffdcf78557ed07552c822ee45c89
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2021-02-05 17:54:35 -0800

    styling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Wats Poppin</h1>
+    <h1>Meet Up</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
